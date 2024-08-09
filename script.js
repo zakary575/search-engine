1bc0391be5b4b75c407a4ceae1e959c5b21a9ac7
Zakary Johnson <zakary.johnson575@gmail.com>
2024-08-08 21:31:18 -0500

    added logic to the event listener if value check happened then to return, and for the search functions to run

---
+++ script.js
@@ -0,0 +1,24 @@
+// a button that when submited will search the input through the two search functions 
+// if the search is empty a modal that tells them to put something in
+// if we add a second page make it send them to the new page
+
+
+const searchBox = document.querySelector(
+    // searchbox id here
+)
+
+const valueCheck = function(){
+    if( searchBox.value === ''){
+        // modal
+    }
+}
+
+
+addEventListener('submit', function(event){
+    event.preventDefault();
+    if (valueCheck){
+        return;
+    }
+    fetchYoutubeVideos();
+    fetchWikiArcticles()
+})
